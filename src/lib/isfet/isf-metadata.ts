e2700d533f065e6583d89f09c63b2c302b59a8fb
Kanon Kakuno <yadex205@outlook.jp>
2024-03-22 19:22:59 +0900

    :+1: Implement ISF metadata parser and support bool,long,float,color,image input

---
+++ src/lib/isfet/isf-metadata.ts
@@ -0,0 +1,226 @@
+import tokenizeGlsl from "glsl-tokenizer/string";
+import * as yup from "yup";
+
+const Vector4Schema = yup.tuple([
+  yup.number().required(),
+  yup.number().required(),
+  yup.number().required(),
+  yup.number().required(),
+]);
+
+const IsfMetadataJsonBaseInputSchema = yup.object({
+  NAME: yup.string().required().matches(/^\S+$/),
+  LABEL: yup.string().optional(),
+  TYPE: yup.string().required().oneOf(["bool", "long", "float", "color", "image"]),
+});
+
+const IsfMetadataJsonBoolInputSchema = IsfMetadataJsonBaseInputSchema.concat(yup.object({
+  DEFAULT: yup.number().optional(),
+}));
+
+type IsfMetadataJsonBoolInput = Omit<yup.InferType<typeof IsfMetadataJsonBoolInputSchema>, "TYPE"> & { TYPE: "bool" };
+
+const IsfMetadataJsonLongInputSchema = IsfMetadataJsonBaseInputSchema.concat(yup.object({
+  DEFAULT: yup.number().optional(),
+  MIN: yup.number().optional(),
+  MAX: yup.number().optional(),
+}));
+
+type IsfMetadataJsonLongInput = Omit<yup.InferType<typeof IsfMetadataJsonLongInputSchema>, "TYPE"> & { TYPE: "long" };
+
+const IsfMetadataJsonFloatInputSchema = IsfMetadataJsonBaseInputSchema.concat(yup.object({
+  DEFAULT: yup.number().optional(),
+  MIN: yup.number().optional(),
+  MAX: yup.number().optional(),
+}));
+
+type IsfMetadataJsonFloatInput = Omit<yup.InferType<typeof IsfMetadataJsonFloatInputSchema>, "TYPE"> & { TYPE: "float" };
+
+const IsfMetadataJsonColorInputSchema = IsfMetadataJsonBaseInputSchema.concat(yup.object({
+  DEFAULT: Vector4Schema.optional(),
+  MIN: Vector4Schema.optional(),
+  MAX: Vector4Schema.optional(),
+}));
+
+type IsfMetadataJsonColorInput = Omit<yup.InferType<typeof IsfMetadataJsonColorInputSchema>, "TYPE"> & { TYPE: "color" };
+
+type IsfMetadataJsonImageInput = Omit<yup.InferType<typeof IsfMetadataJsonColorInputSchema>, "TYPE"> & { TYPE: "image" };
+
+const IsfMetadataJsonInputSchema = yup.lazy(value => {
+  switch (value?.TYPE) {
+    case "bool": return IsfMetadataJsonBoolInputSchema;
+    case "long": return IsfMetadataJsonLongInputSchema;
+    case "float": return IsfMetadataJsonFloatInputSchema;
+    case "color": return IsfMetadataJsonColorInputSchema;
+    case "image": return IsfMetadataJsonBaseInputSchema;
+    default: return IsfMetadataJsonBaseInputSchema;
+  }
+})
+
+const IsfMetadataJsonSchema = yup.object({
+  ISFVSN: yup.string().optional(),
+  VSN: yup.string().optional(),
+  DESCRIPTION: yup.string().optional(),
+  CREDIT: yup.string().optional(),
+  CATEGORIES: yup.array().of(yup.string().required()).optional(),
+  INPUTS: yup.array().of(IsfMetadataJsonInputSchema).optional(),
+});
+
+type IsfMetadataJson = Omit<yup.InferType<typeof IsfMetadataJsonSchema>, "INPUTS"> & {
+  INPUTS?: (
+    | IsfMetadataJsonBoolInput
+    | IsfMetadataJsonLongInput
+    | IsfMetadataJsonFloatInput
+    | IsfMetadataJsonColorInput
+    | IsfMetadataJsonImageInput
+  )[];
+}
+
+interface IsfBaseInput {
+  name: string;
+  label?: string;
+  type: string;
+}
+
+interface IsfBoolInput extends IsfBaseInput {
+  type: "bool";
+  default?: number;
+}
+
+interface IsfLongInput extends IsfBaseInput {
+  type: "long";
+  default?: number;
+  min?: number;
+  max?: number;
+}
+
+interface IsfFloatInput extends IsfBaseInput {
+  type: "float";
+  default?: number;
+  min?: number;
+  max?: number;
+}
+
+interface IsfColorInput extends IsfBaseInput {
+  type: "color";
+  default?: [number, number, number, number];
+  min?: [number, number, number, number];
+  max?: [number, number, number, number];
+}
+
+interface IsfImageInput extends IsfBaseInput {
+  type: "image";
+}
+
+type IsfInput =
+  | IsfBoolInput
+  | IsfLongInput
+  | IsfFloatInput
+  | IsfColorInput
+  | IsfImageInput;
+
+export class IsfMetadata {
+  private _isfVersion?: string;
+  private _version?: string;
+  private _description?: string;
+  private _credit?: string;
+  private _categories: string[] = [];
+  private _inputs: IsfInput[] = [];
+
+  public static parseIsfSource(isfSource: string) {
+    for (const token of tokenizeGlsl(isfSource)) {
+      if (token.type === "whitespace") {
+        continue;
+      } else if (token.type === "block-comment") {
+        const isfMetadataJson = JSON.parse(token.data.replace(/(^\/\*|\*\/$)/g, ""));
+        const isfMetadata = new IsfMetadata();
+
+        isfMetadata.parseIsfMetadataJson(isfMetadataJson);
+
+        return isfMetadata;
+      } else {
+        return null;
+      }
+    }
+
+    return null;
+  }
+
+  public get isfVersion() {
+    return this._isfVersion;
+  }
+
+  public get version() {
+    return this._version;
+  }
+
+  public get description() {
+    return this._description;
+  }
+
+  public get credit() {
+    return this._credit;
+  }
+
+  public get categories() {
+    return this._categories;
+  }
+
+  public get inputs() {
+    return this._inputs;
+  }
+
+  public parseIsfMetadataJson(isfMetadataJson: IsfMetadataJson) {
+    IsfMetadataJsonSchema.validateSync(isfMetadataJson);
+
+    this._isfVersion = isfMetadataJson.ISFVSN;
+    this._version = isfMetadataJson.VSN;
+    this._description = isfMetadataJson.DESCRIPTION;
+    this._credit = isfMetadataJson.CREDIT;
+    this._categories = isfMetadataJson.CATEGORIES || [];
+
+    this._inputs = (isfMetadataJson.INPUTS || []).map(rawInput => {
+      switch (rawInput.TYPE) {
+        case "bool": return {
+          name: rawInput.NAME,
+          label: rawInput.LABEL,
+          type: rawInput.TYPE,
+          default: rawInput.DEFAULT,
+        };
+
+        case "long": return {
+          name: rawInput.NAME,
+          label: rawInput.LABEL,
+          type: rawInput.TYPE,
+          default: rawInput.DEFAULT,
+          min: rawInput.MIN,
+          max: rawInput.MAX,
+        };
+
+        case "float": return {
+          name: rawInput.NAME,
+          label: rawInput.LABEL,
+          type: rawInput.TYPE,
+          default: rawInput.DEFAULT,
+          min: rawInput.MIN,
+          max: rawInput.MAX,
+        };
+
+        case "color": return {
+          name: rawInput.NAME,
+          label: rawInput.LABEL,
+          type: rawInput.TYPE,
+          default: rawInput.DEFAULT,
+          min: rawInput.MIN,
+          max: rawInput.MAX,
+        };
+
+        case "image": return {
+          name: rawInput.NAME,
+          label: rawInput.LABEL,
+          type: rawInput.TYPE,
+        }
+      }
+    });
+  }
+}
---
+++ tests/lib/isfet/isf-metadata.test.ts
@@ -0,0 +1,102 @@
+import { describe, expect, it } from "vitest";
+import { IsfMetadata } from "../../../src/lib/isfet/isf-metadata";
+
+describe("IsfMetadata", () => {
+  describe(".parseIsfMetadataJson", () => {
+    describe("when DESCRIPTION is given", () => {
+      it("updates .description", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ DESCRIPTION: "sample description" });
+
+        expect(isfMetadata.description).to.equal("sample description");
+      });
+    });
+
+    describe("when CREDIT is given", () => {
+      it("updates .credit", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ CREDIT: "Kanon Kakuno" });
+
+        expect(isfMetadata.credit).to.equal("Kanon Kakuno");
+      });
+    });
+
+    describe("when ISFVSN is given", () => {
+      it("updates .isfVersion", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ ISFVSN: "2.0" });
+
+        expect(isfMetadata.isfVersion).to.equal("2.0");
+      });
+    });
+
+    describe("when VSN is given", () => {
+      it("updates .version", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ VSN: "1.0" });
+
+        expect(isfMetadata.version).to.equal("1.0");
+      });
+    });
+
+    describe("when CATEGORIES is given", () => {
+      it("updates .categories", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ CATEGORIES: ["GENERATOR", "Retro"] });
+
+        expect(isfMetadata.categories).toMatchObject(["GENERATOR", "Retro"]);
+      });
+    });
+
+    describe("when bool input is given", () => {
+      it("updates .inputs", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ INPUTS: [{ TYPE: "bool", NAME: "value1" }] });
+
+        expect(isfMetadata.inputs[0]).toMatchObject({ type: "bool", name: "value1" });
+      });
+    });
+
+    describe("when bool input with label is given", () => {
+      it("updates .inputs", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ INPUTS: [{ TYPE: "bool", NAME: "value1", LABEL: "Value 01" }] });
+
+        expect(isfMetadata.inputs[0]).toMatchObject({ type: "bool", name: "value1", label: "Value 01" });
+      });
+    });
+
+    describe("when bool input with default is given", () => {
+      it("updates .inputs", () => {
+        const isfMetadata = new IsfMetadata();
+        isfMetadata.parseIsfMetadataJson({ INPUTS: [{ TYPE: "bool", NAME: "value1", DEFAULT: 1.0 }] });
+
+        expect(isfMetadata.inputs[0]).toMatchObject({ type: "bool", name: "value1", default: 1.0 });
+      });
+    });
+
+    describe("when input's name contains whitespaces", () => {
+      it("throws validation error", () => {
+        const isfMetadata = new IsfMetadata();
+
+        expect(() => isfMetadata.parseIsfMetadataJson({ INPUTS: [{ TYPE: "bool", NAME: "value 1" }] })).toThrowError();
+      });
+    });
+
+    describe("when input doesn't have name", () => {
+      it("throws validation error", () => {
+        const isfMetadata = new IsfMetadata();
+
+        expect(() => isfMetadata.parseIsfMetadataJson({ INPUTS: [{ TYPE: "bool" }] })).toThrowError();
+      });
+    });
+
+    describe("when input doesn't have type", () => {
+      it("throws validation error", () => {
+        const isfMetadata = new IsfMetadata();
+
+        expect(() => isfMetadata.parseIsfMetadataJson({ INPUTS: [{ NAME: "value1" }] })).toThrowError();
+      });
+    });
+  });
+});
