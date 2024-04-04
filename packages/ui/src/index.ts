d93859d9af4ed4297b8d2bfedb95382ee3b74aa1
Kanon Kakuno <yadex205@outlook.jp>
2024-04-04 13:06:03 +0900

    :bug: export types and fix build config

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,7 @@
+import "./ka-color-wheel";
 import "./ka-knob";
+import "./ka-isf-canvas";
+
+export type { KaColorWheel } from "./ka-color-wheel";
+export type { KaKnob } from "./ka-knob";
+export type { KaIsfCanvas } from "./ka-isf-canvas";
